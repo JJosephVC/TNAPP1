aa339333bc494f181a921cdb1a920a4e1e545cef
Joseph Valdivia Chavarría <josephvaldivia1908@gmail.com>
2024-11-21 09:23:21 -0600

    Actu del día

--- TNAPP/Views/EstudiantePage.xaml.cs
+++ TNAPP/Views/EstudiantePage.xaml.cs
@@ -1,5 +1,6 @@
 using MauiPopup;
 using ProTCE.Controllers;
+using ProTCE.Models;
 
 namespace TNAPP.Views;
 
@@ -45,5 +46,18 @@ public partial class EstudiantePage : TabbedPage
     private void OnFloatingButtonTapped(object sender, TappedEventArgs e)
     {
         PopupAction.DisplayPopup(new UnirseModal(userId)); // Abre la modal o ventana emergente
+    }
+
+    private async void OnClaseSelected(object sender, SelectionChangedEventArgs e)
+    {
+        // Verifica si hay un elemento seleccionado
+        if (e.CurrentSelection.FirstOrDefault() is Clase clase)
+        {
+            // Navegar a la página de evaluaciones, pasando los parámetros necesarios
+            await Navigation.PushAsync(new EvaluacionesEstPage(clase.CodigoClase, userId));
+
+            // Limpia la selección para permitir futuras selecciones
+            ((CollectionView)sender).SelectedItem = null;
+        }
     }
 }
